bbcdefbc1772fea4656a21c208ae5d0312b99813
amanuelmandefro2 <128055581+amanuelmandefro2@users.noreply.github.com>
2023-10-24 09:36:11 +0300

    Create 1603-design-parking-system.py

---
+++ 1603-design-parking-system.py
@@ -0,0 +1,12 @@
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.nSlots = [big, medium, small]
+
+    def addCar(self, carType: int) -> bool:
+        if self.nSlots[carType - 1] > 0:
+            self.nSlots[carType - 1] -= 1
+            return True
+        else:
+            return False
+        
