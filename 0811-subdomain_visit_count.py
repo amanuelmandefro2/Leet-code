4bb384a0b06983d908eaa5f8bd84a014bb2d4863
amanuelmandefro2 <128055581+amanuelmandefro2@users.noreply.github.com>
2023-11-11 14:00:04 +0300

    Create 0811-subdomain_visit_count.py

---
+++ 0811-subdomain_visit_count.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+
+        visit_count = {}
+        res = []
+
+        for cpdomain in cpdomains:
+            count, s = cpdomain.split(' ') 
+            count = int(count)
+            for i in range(len(s)-1, -1, -1):
+                if s[i] == '.':
+                    visit_count[s[i+1:]] = visit_count.get(s[i+1:], 0) + count
+            visit_count[s] = visit_count.get(s, 0) + count
+        for link, count in visit_count.items():
+            res.append(str(count) +" "+ str(link))
+        return res
