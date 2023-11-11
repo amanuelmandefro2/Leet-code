73b94a048e26b5f618d0130228ea8159a426e0c1
amanuelmandefro2 <128055581+amanuelmandefro2@users.noreply.github.com>
2023-11-11 12:05:40 +0300

    Create 2109-adding_spaces_to_string.py

---
+++ 2109-adding_spaces_to_string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def addSpaces(self, s: str, spaces: List[int]) -> str:
+        res = s[:spaces[0]]
+        res += ' '
+        i = 1
+        while i < len(spaces):
+            res += s[spaces[i-1]:spaces[i]]
+            res += ' '
+            i+=1
+        i -= 1
+        res += s[spaces[i]:]
+        return res
+        
