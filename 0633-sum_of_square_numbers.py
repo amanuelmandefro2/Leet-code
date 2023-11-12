86a6d878322e1f26f1b61c76b56280985cbae323
amanuelmandefro2 <128055581+amanuelmandefro2@users.noreply.github.com>
2023-11-12 11:29:17 +0300

    Create 0633-sum_of_square_numbers.py

---
+++ 0633-sum_of_square_numbers.py
@@ -0,0 +1,13 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+
+        l, r = 0, int(c ** 0.5)
+
+        while l <= r:
+            if l**2 + r**2 > c:
+                r -= 1
+            elif l**2 + r**2 < c:
+                l += 1
+            elif l**2 + r**2 == c:
+                return True
+        return False
