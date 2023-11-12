1628bed85289426b98a7a98228624362e648accd
amanuelmandefro2 <128055581+amanuelmandefro2@users.noreply.github.com>
2023-11-12 14:26:54 +0300

    Create 0643-maximum_average.py

---
+++ 0643-maximum_average.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        currSum = sum(nums[:k])
+        maxAvg = currSum/k
+        
+        for i in range(k, len(nums)):
+            currSum += nums[i] - nums[i - k] 
+            maxAvg = max(maxAvg, currSum/k)
+        return maxAvg
