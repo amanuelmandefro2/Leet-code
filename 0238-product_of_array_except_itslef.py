1985ed3d0a5d26ea92298ef6de25d64681c12c0a
amanuelmandefro2 <128055581+amanuelmandefro2@users.noreply.github.com>
2023-11-13 11:10:02 +0300

    Update 0238-product_of_array_except_itslef.py

--- 0238-product_of_array_except_itslef.py
+++ 0238-product_of_array_except_itslef.py
@@ -1,17 +1,14 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         answer = [1]*len(nums)
-        l, r = 0, len(nums) -1
-        while r >= 0 and l < len(nums):
-            if l == r:
-                continue
-            else:
-                answer[l] *= nums[r]
-            if r == 0 and l < len(n):
-                l+=1
-                r = len(nums) -1
-            else:
-                r -= 1
 
-
+        prefix = 1
+        for i in range(len(nums)):
+            answer[i] *= prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums)-1, -1, -1 ):
+            answer[i] *= postfix
+            postfix *= nums[i]
         return answer
+
