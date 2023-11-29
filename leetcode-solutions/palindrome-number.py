573b15ebbedb9d876e10e99a77c5d189460ae0ca
amanuelmandefro2 <128055581+amanuelmandefro2@users.noreply.github.com>
2023-11-29 21:53:53 +0300

    Added solution for Palindrome Number

--- leetcode-solutions/palindrome-number.py
+++ leetcode-solutions/palindrome-number.py
@@ -3,11 +3,10 @@ class Solution:
 
         res = 0
         num = x
-
         while num > 0:
-            res = res *10 + num % 10
+            last_digit = num % 10
+            res = res *10 + last_digit
             num //= 10
-            
         return res == x 
             
         
