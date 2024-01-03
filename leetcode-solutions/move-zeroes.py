72d50120b5ebd64b5c555da8b57fe1242e64e419
amanuelmandefro2 <128055581+amanuelmandefro2@users.noreply.github.com>
2024-01-03 13:07:55 +0300

    Added solution for Move Zeroes

---
+++ leetcode-solutions/move-zeroes.py
@@ -0,0 +1,15 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zeroCounter = nums.count(0)
+        nextNonZero = 0
+
+        for n in nums:
+            if n != 0:
+                nums[nextNonZero] = n
+                nextNonZero += 1
+        for zero in range (1, zeroCounter + 1):
+            nums[-zero] = 0
+            
